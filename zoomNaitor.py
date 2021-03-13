3e6ab9b2f8713f9059a5091796f9da708c418a53
Tomás <80177664+TomasVPerez@users.noreply.github.com>
2021-03-13 19:35:48 -0300

    Update zoomNaitor.py

--- zoomNaitor.py
+++ zoomNaitor.py
@@ -4,12 +4,10 @@
 from selenium import webdriver
 import pyautogui, time, subprocess
 
-urlFisica = "https://ortuy.zoom.us/j/95005927377?pwd=Y0F6dVZTL3MyMmhrcGJDQzk4TThCZz09#success"
-urlProg = "https://ortuy.zoom.us/j/98456775707?pwd=Tk5WZEhwS3YvUlBGMmo3OGx1UHNXUT09#success"
-urlFund = "https://ortuy.zoom.us/j/99791591627?pwd=UHcwUkNGaDFvcUJ3Q0tmdVRjaWtKZz09#success"
+urlFisica = #urls de las clases de zoom. 
 
 opciones = webdriver.ChromeOptions()
-opciones.add_experimental_option('excludeSwitches', ['enable-logging'])
+opciones.add_experimental_option('excludeSwitches', ['enable-logging']) #Saca alertas molestas de la consola.
 chrome = webdriver.Chrome(options=opciones)
 
 def iniciarClase(url):
@@ -17,7 +15,7 @@ def iniciarClase(url):
     inicio = chrome.find_element_by_xpath('//*[@id="zoom-ui-frame"]/div[2]/div/div[1]/div')
     inicio.click()
     time.sleep(2)
-    pyautogui.press("tab")
+    pyautogui.press("tab") #Se apreta el tabulador dos veces y el enter para aceptar el popup.
     pyautogui.press("tab")
     pyautogui.press("enter")
 
@@ -27,21 +25,10 @@ horaExacta = fechaActual.split(" ")[3]
 hora = horaExacta.split(":")[0]
 
 while True:
-    if dia == "Mon" and hora == "08" or dia == "Tue" and hora == "10" or dia == "Fri" and hora == "08":
-        iniciarClase(urlProg)
-        time.sleep(10)
-        chrome.close()
-        break
-    if dia == "Mon" and hora == "10" or dia == "Thu" and hora == "10":
-        iniciarClase(urlFund)
-        time.sleep(10)
-        chrome.close()
-        break
-    if dia == "Tue" and hora == "08" or dia == "Wed" and hora == "10": 
+    if dia == "Mon" and hora == "15" or dia == "Tue" and hora == "10" or dia == "Fri" and hora == "08": #Copiar y pegar para agregar materias. Cambiar los dias y horarios.
         iniciarClase(urlFisica)
-        time.sleep(10)
+        time.sleep(10) #Espera 10 segundos y cierra la pestaña
         chrome.close()
-        #subprocess.call("taskkill /f /im py.exe", shell=True)
         break
     else:
         chrome.close()
